7d849aff33181da810cb4ba01e32b636337f5136
B.Suresh <bskumawat3132@gmail.com>
2021-07-26 17:36:22 +0530

    Added views & CRUD routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -19,15 +20,56 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
+// home route
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home.ejs');
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', price: '200', location: 'Lorem Augs, US' })
-    await camp.save();
-    res.send(camp);
+// index route
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index.ejs', { campgrounds });
+})
+
+// add new (form) route
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new.ejs');
+})
+
+// add new route
+app.post('/campgrounds', async (req, res) => {
+    const newCamp = await Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp._id}`);
+})
+
+// show details route
+app.get('/campgrounds/:id', async (req, res) => {
+    const camp = await Campground.findById(req.params.id);
+    res.render('campgrounds/show.ejs', { camp });
+})
+
+// edit (form) route
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const camp = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { camp });
+})
+
+// edit route
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${camp._id}`);
+})
+
+// delete route
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 
 app.listen(3000, (req, res) => {
